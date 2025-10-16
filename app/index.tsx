295bb62bad27132c21eba0a4ddad47c2e904737a
bukurosos <bukurosos@gmail.com>
2025-10-16 15:24:42 +0300

    feat: add login screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,24 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import LoginForm from "@/components/login-form"
 import useAuth from "@/stores/auth";
 import { useEffect } from "react";
+import { useRouter } from "expo-router";
 
 export default function Index() {
   const authStore = useAuth()
+  const router = useRouter()
 
   useEffect(() => {
     console.log('user', authStore.user)
   }, [authStore.user])
 
+  useEffect(() => {
+    setTimeout(() => router.navigate('/login'), 500)
+  })
+
   return (
     <View style={styles.container}>
+      <Text>Hello</Text>
       <LoginForm />
     </View>
   );
