235dbf9c2c1e6504ba334e1c3d4584b9c4921a8b
bukurosos <bukurosos@gmail.com>
2025-10-16 14:03:30 +0300

    feat: implement login & logout store methods

--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -1,5 +1,20 @@
 import { create } from "zustand";
+import { LoginDto } from "../types/login-dto";
+import { User } from "../types/user";
 
-const useAuth = create((set) => ({ user: null }));
+interface AuthState {
+  user: null | User;
+  login: (dto: LoginDto) => void;
+  logout: () => void;
+}
+
+const useAuth = create<AuthState>()((set) => ({
+  user: null,
+  login: (dto: LoginDto) =>
+    set(() => ({
+      user: { email: dto.email },
+    })),
+  logout: () => set(() => ({ user: null })),
+}));
 
 export default useAuth;
