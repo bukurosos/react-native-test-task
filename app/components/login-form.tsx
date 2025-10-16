2ac4a78f2f9e396ac5f223359763d59309012c75
bukurosos <bukurosos@gmail.com>
2025-10-16 13:18:24 +0300

    refactor: move form component into separate file

---
+++ app/components/login-form.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { Text, View, TextInput, Pressable, StyleSheet } from "react-native";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+
+  const onSubmit = () => {
+    console.log(email, password)
+  }
+
+  return (
+    <View style={styles.form}>
+      <Text style={styles.title}>Login</Text>
+
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>Email</Text>
+        <TextInput value={email} onChangeText={setEmail} style={styles.input} keyboardType="email-address" />
+      </View>
+
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>Password</Text>
+        <TextInput value={password} onChangeText={setPassword} style={styles.input} secureTextEntry />
+      </View>
+
+      <Pressable style={styles.button} onPress={onSubmit}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  form: {
+    width: "80%",
+    alignItems: "center",
+    gap: 12,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "600",
+  },
+  inputGroup: {
+    width: "100%",
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 4,
+  },
+  input: {
+    borderColor: "#CFD4DA",
+    borderWidth: 2,
+    borderRadius: 15,
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "black",
+    borderRadius: 15,
+    padding: 12,
+    marginTop: 10,
+    width: "100%",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "600",
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,11 @@
-import { useState } from "react";
-import { Text, View, TextInput, Pressable, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import LoginForm from "./components/login-form"
 
 export default function Index() {
-  const [email, setEmail] = useState<string>("")
-  const [password, setPassword] = useState<string>("")
-
-  const onSubmit = () => {
-    console.log(email, password)
-  }
 
   return (
     <View style={styles.container}>
-      <View style={styles.form}>
-        <Text style={styles.title}>Login</Text>
-
-        <View style={styles.inputGroup}>
-          <Text style={styles.label}>Email</Text>
-          <TextInput value={email} onChangeText={setEmail} style={styles.input} keyboardType="email-address" />
-        </View>
-
-        <View style={styles.inputGroup}>
-          <Text style={styles.label}>Password</Text>
-          <TextInput value={password} onChangeText={setPassword} style={styles.input} secureTextEntry />
-        </View>
-
-        <Pressable style={styles.button} onPress={onSubmit}>
-          <Text style={styles.buttonText}>Submit</Text>
-        </Pressable>
-      </View>
+      <LoginForm />
     </View>
   );
 }
@@ -38,39 +16,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#fff",
-  },
-  form: {
-    width: "80%",
-    alignItems: "center",
-    gap: 12,
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: "600",
-  },
-  inputGroup: {
-    width: "100%",
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 4,
-  },
-  input: {
-    borderColor: "#CFD4DA",
-    borderWidth: 2,
-    borderRadius: 15,
-    padding: 10,
-  },
-  button: {
-    backgroundColor: "black",
-    borderRadius: 15,
-    padding: 12,
-    marginTop: 10,
-    width: "100%",
-  },
-  buttonText: {
-    color: "white",
-    textAlign: "center",
-    fontWeight: "600",
   },
 });
