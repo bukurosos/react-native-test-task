e705d50175223a31a665e3b7be09f0133abeaf00
bukurosos <bukurosos@gmail.com>
2025-10-16 15:30:43 +0300

    feat: improve login view

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,15 @@
 import { View, StyleSheet, Text } from "react-native";
 import LoginForm from "@/components/login-form"
 import useAuth from "@/stores/auth";
-import { useEffect } from "react";
 import { useRouter } from "expo-router";
 
 export default function Index() {
   const authStore = useAuth()
   const router = useRouter()
 
-  useEffect(() => {
-    console.log('user', authStore.user)
-  }, [authStore.user])
-
-  useEffect(() => {
-    setTimeout(() => router.navigate('/login'), 500)
-  })
-
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      {authStore.user && <Text>Hello {authStore.user.email}</Text>}
       <LoginForm />
     </View>
   );
--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,5 +1,18 @@
 import LoginForm from "@/components/login-form"
+import { StyleSheet, View } from "react-native"
 
 export default function Login() {
-  return (<LoginForm />)
+  return (
+    <View style={styles.container}>
+      <LoginForm />
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
