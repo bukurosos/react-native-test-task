57b81d65d3894fdda392cce9fa896dc1bf361268
bukurosos <bukurosos@gmail.com>
2025-10-16 16:43:12 +0300

    feat: improve main screen ui

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,7 @@ import { View, StyleSheet, Text, Pressable } from "react-native";
 import { useAuth } from "@/stores/auth";
 import { useProfiles } from "@/stores/profiles";
 import { useEffect } from "react";
+import Profile from '@/components/profile'
 
 export default function Index() {
   const authStore = useAuth()
@@ -16,12 +17,18 @@ export default function Index() {
     <View style={styles.container}>
       {authStore.user && <View>
         <View style={styles.header}>
+          <Text>{authStore.user.email}</Text>
+
           <Pressable onPress={logout} style={styles.logoutButton}>
             <Text style={styles.logoutText}>Logout</Text>
           </Pressable>
         </View>
 
-        <Text>Hello {authStore.user.email}</Text>
+        <View style={styles.profilesContainer}>
+          <Text style={styles.profileTitle}>Profiles</Text>
+
+          {profileStore.profiles && profileStore.profiles.map(profile => (<Profile profile={profile} key={profile.id.value} />))}
+        </View>
       </View>}
     </View>
   );
@@ -31,20 +38,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    paddingHorizontal: 10
   },
   header: {
     paddingHorizontal: 10,
-    marginTop: 15
+    marginTop: 15,
+    justifyContent: 'space-between',
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   logoutButton: {
     backgroundColor: 'black',
     paddingHorizontal: 13,
     paddingVertical: 8,
     borderRadius: 15,
-    alignSelf: 'flex-end'
   },
   logoutText: {
     fontSize: 18,
     color: 'white'
+  },
+  profilesContainer: {
+    marginTop: 15,
+  },
+  profileTitle: {
+    fontSize: 25,
+    alignSelf: 'center',
+    marginBottom: 15
   }
 });
